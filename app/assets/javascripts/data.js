e469a79a4f6b9d99aba814b7df74e29a2447751d
Thanh Huynh <thanhjamin@yahoo.com>
2014-01-18 21:05:57 -0800

    first commit

---
+++ app/assets/javascripts/data.js
@@ -0,0 +1,35 @@
+function getHotels(searchValue){
+  if (searchValue.length == 0) {
+    searchValue = undefined;
+    alert("Please enter in a city");
+  }
+
+  var req = $.ajax({
+        type: "GET",
+        url: "http://jsonp.jit.su/?callback=displayAll&url=http%3A%2F%2Fdeals.expedia.com%2Fbeta%2Fdeals%2Fhotels.json%3Fcity%3D" + searchValue,
+        dataType: "JSONP",
+        success: displayAll,
+        error: function(jqXHR, textStatus, errorThrown){
+          console.log(jqXHR, textStatus, errorThrown);
+        }
+
+  });
+}
+
+function displayAll(data){
+  console.log(data)
+  $.each(data, function(key, value){
+    $(" body").append("<div><span>"+value['city']+":</span><span>"+value['starRating']+"</span></div>");
+  })
+}
+
+$(document).ready(function() {
+  document.getElementById('button').onclick = function(e, searchValue){
+    e.preventDefault();
+    searchValue = $('#submit').val();
+    getHotels(searchValue);
+
+  }
+});
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HotelDeals::Application.config.secret_key_base = 'e451978b252241f92983ea99b4f2ea53acc8e099c232b345e562c8846913eed92cbea143efee185ecb161c5b4312a93cb280c93a8363620b5933b1279d3e5688'
